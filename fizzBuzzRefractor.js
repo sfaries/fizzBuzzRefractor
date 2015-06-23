36a4f0e9c79c0fd21408adf8a1e8a4c10cda3738
sfaries <shaunfaries@gmail.com>
2015-06-23 18:16:49 -0400

    Initial commit

--- fizzBuzzRefractor.js
+++ fizzBuzzRefractor.js
@@ -1,17 +1,15 @@
+/*
 $(document).ready(function(){
 
 // Variables
-var $integer = $('#input').val(+""); //can i chain this with parseInt();?
 
-//listen for text input
-	$('#input').on('keypress', function(Refractor));
-
+var integer = prompt(+'');
 //Convert string into integer
-	//$("#input").parseInt();
+	//this.parseInt();
 
 //Named Function accepting integer as input
-	Function Refractor($integer){
-		for (var i = 1; i == $integer; i++){
+	function Refractor(integer){
+		for (var i = 1; i == integer; i++){
 			document.write("<br />");
 
 			if ((i % 3 === 0) && (i % 5 === 0)){
@@ -29,25 +27,7 @@ var $integer = $('#input').val(+""); //can i chain this with parseInt();?
 		}
 	};
 
-//Code for FizzBuzz
-/*
-	for (var i = 1; i < 101; i++) 
-		{
-			document.write("<br />");
-
-			if ((i % 3 == 0) && (i % 5 == 0)) {
-			document.write('fizzbuzz');
-			} 
-			else if ((i % 3 == 0) && (i % 5 != 0)) {
-			document.write('fizz'); 
-			} 
-			else if ((i % 5 == 0) && (i % 3 != 0)) {
-			document.write('buzz'); 
-			} 
-			else {
-			document.write(i); 
-			}
-		}
-	*/
+	Refractor(integer);
 
 });
+*/
